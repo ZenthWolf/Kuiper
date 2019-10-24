05894764a066c3f089820fa5fdf8449f66261012
zenthwolf <zenthrelin@gmail.com>
2019-10-23 19:57:53 -0500

    Included vector logic. Oops, going to have to update a lot of stuff...

---
+++ BreezeGameEngine/Vec.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class Vec
+{
+public:
+	Vec();
+	Vec(float x0, float y0);
+	float GetLength() const;
+	float GetLengthSq() const;
+	Vec operator+(const Vec& rhs) const;
+	Vec& operator+=(const Vec& rhs);
+	Vec operator-(const Vec& rhs) const;
+	Vec& operator-=(const Vec& rhs);
+	Vec operator*(const float scale) const;
+
+public:
+	float X;
+	float Y;
+};
