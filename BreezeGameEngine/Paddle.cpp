8391e35e0e56becbf56efd962e5f835d44c600a5
zenthwolf <zenthrelin@gmail.com>
2019-10-23 23:24:58 -0500

    Paddles introduced with basic behavior

--- BreezeGameEngine/Paddle.cpp
+++ BreezeGameEngine/Paddle.cpp
@@ -26,5 +26,18 @@ void Paddle::Update(Keyboard& kbd, float dt)
 		Rect.Pos.X += Vx * dt * 60.0f;
 	}
 
+/*** Test Rects for Wall Behavior ***/
 	RectF Left(gfx, 0.0f, 0.0f, 5.0f, float(gfx.ScreenHeight), Colors::White);
+	RectF Right(gfx, float(gfx.ScreenWidth) - 5.0f, 0.0f, 5.0f, float(gfx.ScreenHeight), Colors::White);
+//	RectF Top(gfx, 0.0f, 0.0f, float(gfx.ScreenWidth), 5.0f, Colors::White);
+//	RectF Bot(gfx, 0.0f, float(gfx.ScreenHeight) - 5.0f, float(gfx.ScreenWidth), 5.0f, Colors::White);
+
+	if (Rect.CollWith(Left))
+	{
+		Rect.Pos.X = 5.0f;
+	}
+	else if (Rect.CollWith(Right))
+	{
+		Rect.Pos.X = float(gfx.ScreenWidth) - 5.0f - Rect.W;
+	}
 }
